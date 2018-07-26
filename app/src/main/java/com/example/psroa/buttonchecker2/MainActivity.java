5d98c8cade0c5ea4db37e63bcd5bfbf6edef4500
mrlandfried <compscipd@gmail.com>
2018-07-26 14:01:06 -0700

    First Commit

---
+++ app/src/main/java/com/example/psroa/buttonchecker2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.psroa.buttonchecker2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickMeClick(View view) {
+        Toast.makeText(this, "Ouch!", Toast.LENGTH_SHORT).show();
+    }
+}
